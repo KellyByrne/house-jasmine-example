47dae03ba85c45a38ff9c57244fdb853497fbc4c
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-08-07 11:35:52 -0600

    again

--- house.js
+++ house.js
@@ -1,6 +1,20 @@
-function House(numberOfDoors) {
+function House(numberOfDoors, numberOfWindows) {
 	this.numberOfDoors = numberOfDoors;
-
+	this.numberOfWindows = numberOfWindows;
+	this.toString = function() {
+	if(this.numberOfDoors > 1 && this.numberOfWindows === 1) {
+		return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " window";
+	} else if (this.numberOfDoors > 1 && this.numberOfWindows > 1){
+		return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+	} else if (this.numberOfDoors === 1 && this.numberOfWindows > 1) {
+		return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " windows";
+	} else if (this.numberOfDoors === 1 && this.numberOfWindows === 1) {
+		return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " window";
+	} else if (this.numberOfDoors === 0 && this.numberOfWindows === 0) {
+		return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+	}
 }
+}
+
 
 module.exports = House;
